c3c5965bc1113586f7697ce92319d611101b73da
fhz <13718913664@163.com>
2018-11-28 17:38:11 +0800

    commit

---
+++ TxOcr/src/Constats.java
@@ -0,0 +1,18 @@
+/**
+ * @Description:
+ * @Param:
+ * @return:
+ * @Author: fenghouzhi
+ * @Date: 3:27 PM 2018/10/10
+ */
+public class Constats {
+
+    static final String APP_ID = "1256658797";
+
+    static final String SECRET_ID = "AKIDJXdq232lRMBvoXobvI8rVyIWgJuLti8c";
+
+    static final String SECRET_KEY = "5LVwGuNfgPMA91A1rX9aqK2c1JXacgnq";
+
+    static final Integer EXPIRED = 30 * 60 * 1000;
+
+}
